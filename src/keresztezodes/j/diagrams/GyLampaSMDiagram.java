56af980a04f4046714658a14f169e8b608f46fb2
Moocska <48150864+Moocska@users.noreply.github.com>
2019-03-02 10:50:39 +0100

    State, class sorrend
    A GyLampa.txtuml fajl sorrendjéhez igazítottam a classok sorrendjét és ezzel az import sorrendeken is változtatni kellett.

--- src/keresztezodes/j/diagrams/GyLampaSMDiagram.java
+++ src/keresztezodes/j/diagrams/GyLampaSMDiagram.java
@@ -2,9 +2,9 @@ package keresztezodes.j.diagrams;
 
 import keresztezodes.x.model.gyLampa;
 import keresztezodes.x.model.gyLampa.Init;
-import keresztezodes.x.model.gyLampa.Piros;
-import keresztezodes.x.model.gyLampa.ZVillog;
 import keresztezodes.x.model.gyLampa.Zold;
+import keresztezodes.x.model.gyLampa.ZVillog;
+import keresztezodes.x.model.gyLampa.Piros;
 
 import hu.elte.txtuml.api.layout.Row;
 import hu.elte.txtuml.api.layout.StateMachineDiagram;
@@ -14,7 +14,7 @@ public class GyLampaSMDiagram extends StateMachineDiagram<gyLampa> {
 	//Init will be in the first row
 	//The others will be in the second row
 	@Row({ Init.class })
-	@Row({ Piros.class, Zold.class, ZVillog.class })
+	@Row({ Zold.class, ZVillog.class,Piros.class })
 	class CarLay extends Layout {
 	}
 }
